0dbb38fe42cfd6df6fb6c7c149a73c50b8d2ec2f
fiofiorito <124637724+fiofiorito@users.noreply.github.com>
2024-01-27 17:34:46 -0300

    style footer

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
+import Link from 'next/link';
 import './Footer.css';
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     const date = new Date
     const year = date.getFullYear()
 
     return <footer>
-        <div>
-            {/* aca van las imagenes de redes sociales + link a ellas */}
+        <div className='footer-socials'>
+            <Link className='footer-socials-link' target='_blank' href='https://www.linkedin.com/in/fiorella-fiorito/'><FaLinkedin /></Link>
+            <Link className='footer-socials-link' target='_blank' href='https://www.instagram.com/fioffiiorito/'><FaInstagram /></Link>
+            <Link className='footer-socials-link' target='_blank' href='https://github.com/fiofiorito'><FaGithub /></Link>
         </div>
         <p className='footer-p'>iFio © {year}. All rights reserved.</p>
     </footer>
